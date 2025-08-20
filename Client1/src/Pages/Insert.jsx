efa657d26fc5ac9d8d957491b08343dceede8418
praveen choudhary <pc852180@gmail.com>
2025-08-20 18:13:30 +0530

    Crud Data

--- Client1/src/Pages/Insert.jsx
+++ Client1/src/Pages/Insert.jsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import BASE_URL from '../Config/Config';
+import axios from "axios";
 const Insert =()=>{
+
+      const[input,setInput] = useState('');
+
+      const handelInput = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setInput(values=>({...values, [name]:value}))
+        console.log(input);
+
+      }
+
+      const handelSubmit =async(e)=>{
+        e.preventDefault();
+        const api = `${BASE_URL}/Student/InsertData`;
+        try {
+            const response = await axios.post(api, input);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error)
+        }
+      }
+
+
     return(
         <>
         <div id="from">
@@ -8,29 +34,29 @@ const Insert =()=>{
           <Form>
       <Form.Group className="mb-3" controlId="formBasicEmailk">
         <Form.Label>Enter Student Name</Form.Label>
-        <Form.Control type="text"  />
+        <Form.Control type="text" name='name' value={input.name} onChange={handelInput} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmails">
         <Form.Label>Enter Student Email</Form.Label>
-        <Form.Control type="text"  />
+        <Form.Control type="text"  name='email' value={input.email} onChange={handelInput} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmailj">
         <Form.Label>Enter Student Roll No</Form.Label>
-        <Form.Control type="text"  />
+        <Form.Control type="text" name='rollno' value={input.rollno} onChange={handelInput} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmaild">
         <Form.Label>Enter Student City</Form.Label>
-        <Form.Control type="text"  />
+        <Form.Control type="text" name='city' value={input.city} onChange={handelInput}  />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Student Number</Form.Label>
-        <Form.Control type="number"  />
+        <Form.Label>Student Contact Number</Form.Label>
+        <Form.Control type="number" name='number' value={input.number} onChange={handelInput} />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={handelSubmit}>
         Submit
       </Button>
     </Form>
