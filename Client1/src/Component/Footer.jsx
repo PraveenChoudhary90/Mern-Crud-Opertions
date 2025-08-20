194c8818114560492dd95e1da2cb1ad10c659d60
praveen choudhary <pc852180@gmail.com>
2025-08-20 15:16:29 +0530

    Crud Mern

---
+++ Client1/src/Component/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = ()=>{
+    return(
+        <>
+        <h1 style={{padding:"40px", textAlign:"center", backgroundColor:"pink"}}>www.crudopertion.com</h1>
+        </>
+    )
+}
+
+export default Footer;
